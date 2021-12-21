c7cc8a1ac1d647f02c47ea3fcdf19258026d45e8
roman kostyrin <enemy-iubip@mail.ru>
2021-12-22 01:46:19 +0300

    feat: add normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import { store } from './stores/store'
-import { Provider } from 'react-redux'
+import { store } from './stores/store';
+import { Provider } from 'react-redux';
+import { Normalize } from 'styled-normalize';
 
 ReactDOM.render(
   <Provider store={store}>
+    <Normalize />
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
