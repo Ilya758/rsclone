bd5080dff5c622a8f3a1ffff0d72a09ef4ea8f3b
roman kostyrin <enemy-iubip@mail.ru>
2021-12-22 23:28:18 +0300

    fix: fixed merged files

---
+++ src/components/genericButton/genericButton.types.ts
@@ -0,0 +1,10 @@
+export interface IGenericButton {
+  text?: string,
+  padding: number,
+  hoverColor: string,
+  margin: number,
+  backgroundColor: string,
+  border: string,
+  color: string;
+  cursor: string;
+}
