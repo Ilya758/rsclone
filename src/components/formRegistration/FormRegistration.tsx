4bdfea69f727f4123e6fa942e6c69f7bf6a81cda
roman kostyrin <enemy-iubip@mail.ru>
2021-12-23 16:22:29 +0300

    feat: add error message on form

--- src/components/formRegistration/FormRegistration.tsx
+++ src/components/formRegistration/FormRegistration.tsx
@@ -1,16 +1,27 @@
 import { WrapperStyle, HeaderStyle } from './formRegistration.style';
 import Input from '../inputs/Input';
+import ErrorMessage from '../messages/errorMessage/ErrorMessage';
 
 function FormRegistration() {
   return (
     <WrapperStyle>
-      <HeaderStyle>Enter the Zero World!</HeaderStyle>
+      <HeaderStyle>Enter to the CloneZero!</HeaderStyle>
       <Input type="text" text="Login" placeholder="Enter login" id="login" />
       <Input
         type="password"
         text="Password"
         placeholder="Enter password"
         id="password"
+      />
+      <ErrorMessage
+        text={'wrong login or password'}
+        padding={0}
+        hoverColor={''}
+        margin={5}
+        backgroundColor={'transparent'}
+        color={'red'}
+        border={'none'}
+        cursor={'initial'}
       />
     </WrapperStyle>
   );
