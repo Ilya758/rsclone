52236529732d195d0cfe556f0e5b6fe3b6a4c8ce
roman kostyrin <enemy-iubip@mail.ru>
2021-12-25 03:24:08 +0300

    feat: add click handler on select field

--- src/components/inputs/select/Select.tsx
+++ src/components/inputs/select/Select.tsx
@@ -1,10 +1,10 @@
 import { SelectStyle, LabelStyle } from './select.style';
 import { ISelect } from './select.types';
-const Select = ({ options, id, name }: ISelect) => {
+const Select = ({ options, id, name, changeHandle }: ISelect) => {
   return (
     <>
       <LabelStyle htmlFor={id}>{name}</LabelStyle>
-      <SelectStyle name={name} id={id}>
+      <SelectStyle name={name} id={id} onChange={e => changeHandle(e)}>
         {options.map((optionName, index) => (
           <option value={optionName} key={index}>
             {optionName}
