315c9503101969c47e566b1efa7534f032f49a8f
Ilya758 <subscriber6436@gmail.com>
2022-01-02 19:46:35 +0300

    fix: minor edit in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
-import Game from './components/game/Game';
+import GameInitializer from './components/game/GameInitializer';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="game" element={<Game />} />
+      <Route path="game" element={<GameInitializer />} />
     </Routes>
   );
 }
