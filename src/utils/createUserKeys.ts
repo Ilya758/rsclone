cd860c4af41ce726de869cfe6892752cfea40f02
Ilya758 <subscriber6436@gmail.com>
2022-01-05 23:49:44 +0300

    fix: modified utility method createUserKeys, fixed interface IUserKeys to IUserInteractiveButtons

--- src/utils/createUserKeys.ts
+++ src/utils/createUserKeys.ts
@@ -1,15 +1,19 @@
-import { IUserKeys } from '../types/globals';
+import { IUserInteractiveButtons } from '../types/globals';
 
-export const createUserKeys = (input: Phaser.Input.InputPlugin): IUserKeys => {
+export const createUserKeys = (
+  input: Phaser.Input.InputPlugin
+): IUserInteractiveButtons => {
   const left = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
   const right = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
   const down = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
   const up = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  const mouseButtons = input.mousePointer;
 
   return {
     left,
     right,
     down,
     up,
+    mouseButtons,
   };
 };
