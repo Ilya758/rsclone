04a64686169ddbbad85704ead63619867b7ef9ca
roman kostyrin <enemy-iubip@mail.ru>
2022-01-11 09:57:49 +0300

    feat: add button controlls

--- src/utils/createUserKeys.ts
+++ src/utils/createUserKeys.ts
@@ -7,6 +7,11 @@ export const createUserKeys = (
   const right = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
   const down = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
   const up = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  const one = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
+  const two = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
+  const three = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
+  const four = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR);
+  const five = input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FIVE);
   const mouseButtons = input.mousePointer;
 
   return {
@@ -15,5 +20,10 @@ export const createUserKeys = (
     down,
     up,
     mouseButtons,
+    one,
+    two,
+    three,
+    four,
+    five,
   };
 };
