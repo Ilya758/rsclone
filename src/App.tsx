572d56e4d469ef1c20308ac9aa5689469a54a25d
AlekseyVY <alekseyvy@gmail.com>
2022-01-14 22:55:50 +0300

    fix: multiplayer route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import GameInitializer from './components/game/GameInitializer';
+import MultiplayerInitializer from "./components/game/MultiplayerInitializer";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="game" element={<GameInitializer />} />
+      <Route path="multiplayer" element={<MultiplayerInitializer />} />
     </Routes>
   );
 }
