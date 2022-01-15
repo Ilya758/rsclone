8fa40720d1ceb3974f3e97e894e1e7ef0a41ba81
Ilya758 <subscriber6436@gmail.com>
2022-01-15 22:48:28 +0300

    feat: added zombies-constant and types

---
+++ src/constants/zombies.ts
@@ -0,0 +1,34 @@
+import { IZombies } from '../components/game/enemies/zombie.types';
+
+export const ZOMBIES: IZombies = {
+  femaleZombie: {
+    hp: 100,
+    speed: 60,
+    damage: 8,
+  },
+  femaleZombieBrunet: {
+    hp: 110,
+    speed: 55,
+    damage: 9,
+  },
+  zombie: {
+    hp: 150,
+    speed: 50,
+    damage: 10,
+  },
+  athlete: {
+    hp: 250,
+    speed: 45,
+    damage: 13,
+  },
+  armyZombie: {
+    hp: 300,
+    speed: 50,
+    damage: 10,
+  },
+  policeZombie: {
+    hp: 350,
+    speed: 60,
+    damage: 15,
+  },
+};
