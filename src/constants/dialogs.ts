91a6059c9926620e18582f559d78e9c9646c6910
roman kostyrin <enemy-iubip@mail.ru>
2022-01-18 00:37:16 +0300

    feat: add dialogs

---
+++ src/constants/dialogs.ts
@@ -0,0 +1,45 @@
+export const DIALOGS = [
+  {
+    count: 1,
+    width: 220,
+    height: 120,
+    text: "The latest news! The zombie has been found in a small town - SmallTown! If you'l see anyone, please, be careful! Better stay at home!",
+    coordinates: [5380, 5580],
+    delay: 7000,
+  },
+  {
+    count: 2,
+    width: 120,
+    height: 120,
+    text: "What?.... It's never be true! But I heard something...",
+    delay: 3000,
+  },
+  {
+    count: 3,
+    width: 120,
+    height: 120,
+    text: 'Who are you? What are you doing here?... Omg!!! Die!!!',
+    delay: 3000,
+  },
+  {
+    count: 4,
+    width: 120,
+    height: 120,
+    text: 'I need a better weapon and get out of here!',
+    delay: 3000,
+  },
+  {
+    count: 5,
+    width: 120,
+    height: 60,
+    text: 'Glock! Nice gun!',
+    delay: 3000,
+  },
+  {
+    count: 6,
+    width: 120,
+    height: 120,
+    text: 'This zombie was big! I think our police was destroyed, but I have to find survivals',
+    delay: 4000,
+  },
+];
