e90f28445280b52cd3bc70f3b447b0687c360a61
roman kostyrin <enemy-iubip@mail.ru>
2022-01-18 20:54:19 +0300

    fix: add correct coordinates on icons

--- src/components/game/ui-kit/PersonUi.ts
+++ src/components/game/ui-kit/PersonUi.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import Person from '../person/Person';
 import PersonHealthBar from './health-bars/PersonHealthBar';
+import IconUi from './IconUi';
 
 export default class PersonUI extends Phaser.Scene {
   parentScene: Phaser.Scene;
@@ -38,6 +39,8 @@ export default class PersonUI extends Phaser.Scene {
   }
 
   create() {
+    const xCoord = this.parentScene.scale.width / 2 - 100;
+    const yCoords = this.parentScene.scale.height - 50;
     const emptyItems = this.add.group({
       classType: Phaser.GameObjects.Image,
     });
@@ -45,33 +48,38 @@ export default class PersonUI extends Phaser.Scene {
       setScale: { x: 0.25, y: 0.25 },
       key: 'empty-item',
       setXY: {
-        x: 300,
-        y: 370,
+        x: xCoord,
+        y: yCoords,
         stepX: 60,
       },
       quantity: 5,
     });
-    this.knife = this.add.image(300, 365, 'knife');
+    this.knife = this.add.image(xCoord, yCoords - 5, 'knife');
     this.knife.setScale(0.3, 0.3);
-    this.bat = this.add.image(360, 365, 'bat');
+    this.bat = this.add.image(xCoord + 60, yCoords - 5, 'bat');
     this.bat.setRotation(0.55);
     this.bat.setScale(0.15, 0.15);
-    this.gun = this.add.image(420, 365, 'gun');
+    this.gun = this.add.image(xCoord + 120, yCoords - 5, 'gun');
     this.gun.setRotation(0.45);
     this.gun.setScale(0.3, 0.3);
-    this.rifle = this.add.image(480, 365, 'rifle');
+    this.rifle = this.add.image(xCoord + 180, yCoords - 5, 'rifle');
     this.rifle.setRotation(0.45);
     this.rifle.setScale(0.15, 0.15);
-    this.fire = this.add.image(540, 365, 'firethrower');
+    this.fire = this.add.image(xCoord + 240, yCoords - 5, 'firethrower');
     this.fire.setRotation(0.45);
     this.fire.setScale(0.15, 0.15);
     this.changeWeapon('knife');
+    new IconUi(this);
   }
 
   changeWeapon(type: string) {
     if (this.active) this.active.destroy();
 
-    this.active = this.add.image(this.itemKeys[type], 365, 'active-item');
+    this.active = this.add.image(
+      this.itemKeys[type],
+      this.parentScene.scale.height - 55,
+      'active-item'
+    );
     this.active.setScale(0.13, 0.13);
   }
 }
