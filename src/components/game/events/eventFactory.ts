a4ef1bf04b62f34548408ff137f4d6329d934500
roman kostyrin <enemy-iubip@mail.ru>
2022-01-18 20:53:10 +0300

    feat: add fade event on scene

--- src/components/game/events/eventFactory.ts
+++ src/components/game/events/eventFactory.ts
@@ -1,26 +1,57 @@
 import { COORDINATES } from './../../../constants/coordinates';
+import { QUESTLABELS } from './../../../constants/questLabels';
 import Phaser from 'phaser';
 import DialogBox from '../plot/DialogBox';
 import { DIALOGS } from '../../../constants/dialogs';
 import sceneEvents from './eventCenter';
+import QuestLabel from '../ui-kit/QuestLabel';
 import '../enemies/Zombie';
 export default class EventFactory {
   private roofs: Phaser.Tilemaps.TilemapLayer[];
   private scene: Phaser.Scene;
   private person: Phaser.Physics.Arcade.Sprite;
+  private personUi: Phaser.Scene;
+  private questLabels: QuestLabel[];
 
   constructor(
     scene: Phaser.Scene,
     person: Phaser.Physics.Arcade.Sprite,
-    roofs: Phaser.Tilemaps.TilemapLayer[]
+    roofs: Phaser.Tilemaps.TilemapLayer[],
+    personUi: Phaser.Scene
   ) {
     this.scene = scene;
     this.person = person;
     this.roofs = roofs;
+    this.personUi = personUi;
     this.run();
+    this.questLabels = [];
   }
 
   run() {
+    // sceneEvents.on('killZombieEvent', () => {})
+
+    sceneEvents.on('hide', () => {
+      this.scene.cameras.main.fadeOut(2000);
+      //TODO
+      setTimeout(() => {
+        this.person.setX(400);
+        this.person.setY(400);
+      }, 3000);
+      setTimeout(() => {
+        this.scene.cameras.main.fadeIn(1000);
+      }, 3000);
+    });
+
+    sceneEvents.on('questLabel', (number: number) => {
+      this.questLabels.push(
+        new QuestLabel(this.personUi, QUESTLABELS[number], number, 10, 150)
+      );
+    });
+
+    sceneEvents.on('questLabelDestroy', (number: number) => {
+      this.questLabels[number].crossLine();
+    });
+
     sceneEvents.on('roof', (number: number) => {
       this.scene.tweens.add({
         targets: this.roofs[number],
@@ -35,17 +66,37 @@ export default class EventFactory {
         'zombie'
       );
     });
+    sceneEvents.on('zombies', (number: number) => {
+      this.scene.add.zombie(
+        COORDINATES.zombies[number][0],
+        COORDINATES.zombies[number][1],
+        'zombie'
+      );
+      this.scene.add.zombie(
+        COORDINATES.zombies[number][0],
+        COORDINATES.zombies[number][1],
+        'zombie'
+      );
+      this.scene.add.zombie(
+        COORDINATES.zombies[number][0],
+        COORDINATES.zombies[number][1],
+        'zombie'
+      );
+      //TODO зареспать много мобов
+    });
 
     sceneEvents.on(
       'dialog',
       (number: number) => {
         if (!this.person) throw new Error('error');
-        const coords = DIALOGS[number].coordinates
-          ? DIALOGS[number].coordinates
-          : [this.person.x, this.person.y];
+        const coords = (
+          DIALOGS[number].coordinates
+            ? DIALOGS[number].coordinates
+            : [this.person.x, this.person.y]
+        ) as number[];
         new DialogBox(
-          coords![0] - DIALOGS[number].width / 7,
-          coords![1] -
+          coords[0] - DIALOGS[number].width / 7,
+          coords[1] -
             (DIALOGS[number].height + DIALOGS[number].height / 4 + 10),
           DIALOGS[number].width,
           DIALOGS[number].height,
