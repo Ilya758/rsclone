97030a1c8c797fca10c01b28f2a84d45c89ec1c3
Ilya758 <subscriber6436@gmail.com>
2022-01-31 14:59:27 +0300

    feat: added constant weaponsGraphicsChars

---
+++ src/constants/weaponsGraphicsChars.ts
@@ -0,0 +1,27 @@
+export const weaponsGraphicsChars = [
+  {
+    type: 'knife',
+    rotation: 0,
+    scale: 0.4,
+  },
+  {
+    type: 'bat',
+    rotation: 0.55,
+    scale: 0.25,
+  },
+  {
+    type: 'pistol',
+    rotation: 0.45,
+    scale: 0.4,
+  },
+  {
+    type: 'rifle',
+    rotation: 0.45,
+    scale: 0.22,
+  },
+  {
+    type: 'firethrower',
+    rotation: 0.45,
+    scale: 0.22,
+  },
+];
