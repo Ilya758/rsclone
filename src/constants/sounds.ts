030b24e92f96509425a381eb193b230154edf6a4
Ilya758 <subscriber6436@gmail.com>
2022-02-01 13:58:47 +0300

    feat: added constant sounds

---
+++ src/constants/sounds.ts
@@ -0,0 +1,46 @@
+export const SOUNDS = [
+  {
+    name: 'person-walk',
+    url: './assets/audio/person/person-walk.mp3',
+  },
+  {
+    name: 'person-hit',
+    url: './assets/audio/person/person-hit.mp3',
+  },
+  {
+    name: 'person-dead',
+    url: './assets/audio/person/person-dead.mp3',
+  },
+  {
+    name: 'rifle-shot',
+    url: './assets/audio/weapon/rifle/shot.mp3',
+  },
+  {
+    name: 'first-phrase',
+    url: './assets/audio/person/phrases/first-phrase.mp3',
+  },
+  {
+    name: 'zombie-aggressive',
+    url: './assets/audio/zombie/zombie-aggressive.mp3',
+  },
+  {
+    name: 'zombie-hit',
+    url: './assets/audio/zombie/zombie-hit.mp3',
+  },
+  {
+    name: 'zombie-dead',
+    url: './assets/audio/zombie/zombie-dead.mp3',
+  },
+  {
+    name: 'zombie-horde',
+    url: './assets/audio/zombie/zombie-horde.mp3',
+  },
+  {
+    name: 'track-static',
+    url: './assets/audio/tracks/track-static.mp3',
+  },
+  {
+    name: 'track-dynamic',
+    url: './assets/audio/tracks/track-dynamic.mp3',
+  },
+];
