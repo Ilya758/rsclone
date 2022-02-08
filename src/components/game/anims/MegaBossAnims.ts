208bec5b55d6adc837fbf151d64494c10f4fee1d
Ilya758 <subscriber6436@gmail.com>
2022-02-08 16:38:32 +0300

    fix: fixed animations, atlases

--- src/components/game/anims/MegaBossAnims.ts
+++ src/components/game/anims/MegaBossAnims.ts
@@ -1,22 +1,24 @@
 import Phaser from 'phaser';
 
-const createZombieBossAnims = (anims: Phaser.Animations.AnimationManager) => {
+const createMegaBossAnims = (
+  anims: Phaser.Animations.AnimationManager | Phaser.Animations.AnimationState
+) => {
   anims.create({
     key: 'stay',
-    frames: anims.generateFrameNames('zombie', {
-      start: 6,
+    frames: anims.generateFrameNames('megaBoss', {
+      start: 0,
       end: 8,
       prefix: 'Attack_00',
       suffix: '.png',
     }),
     repeat: -1,
-    frameRate: 4,
+    frameRate: 9,
   });
   anims.create({
     key: 'kick1',
-    frames: anims.generateFrameNames('zombie', {
+    frames: anims.generateFrameNames('megaBoss', {
       start: 0,
-      end: 14,
+      end: 9,
       prefix: 'Attack_1_00',
       suffix: '.png',
     }),
@@ -25,9 +27,9 @@ const createZombieBossAnims = (anims: Phaser.Animations.AnimationManager) => {
   });
   anims.create({
     key: 'kick2',
-    frames: anims.generateFrameNames('zombie', {
+    frames: anims.generateFrameNames('megaBoss', {
       start: 0,
-      end: 8,
+      end: 6,
       prefix: 'Attack_2_00',
       suffix: '.png',
     }),
@@ -36,7 +38,7 @@ const createZombieBossAnims = (anims: Phaser.Animations.AnimationManager) => {
   });
   anims.create({
     key: 'kick3',
-    frames: anims.generateFrameNames('zombie', {
+    frames: anims.generateFrameNames('megaBoss', {
       start: 0,
       end: 8,
       prefix: 'Attack_3_00',
@@ -47,7 +49,7 @@ const createZombieBossAnims = (anims: Phaser.Animations.AnimationManager) => {
   });
   anims.create({
     key: 'kick4',
-    frames: anims.generateFrameNames('zombie', {
+    frames: anims.generateFrameNames('megaBoss', {
       start: 0,
       end: 8,
       prefix: 'Attack_4_00',
@@ -58,7 +60,7 @@ const createZombieBossAnims = (anims: Phaser.Animations.AnimationManager) => {
   });
   anims.create({
     key: 'walk',
-    frames: anims.generateFrameNames('zombie', {
+    frames: anims.generateFrameNames('megaBoss', {
       start: 0,
       end: 7,
       prefix: 'walk_00',
@@ -69,14 +71,14 @@ const createZombieBossAnims = (anims: Phaser.Animations.AnimationManager) => {
   });
   anims.create({
     key: 'zombie-death',
-    frames: anims.generateFrameNames('zombie', {
+    frames: anims.generateFrameNames('megaBoss', {
       start: 0,
       end: 13,
       prefix: 'Death_00',
       suffix: '.png',
     }),
-    frameRate: 5,
+    frameRate: 9,
   });
 };
 
-export { createZombieBossAnims };
+export { createMegaBossAnims };
